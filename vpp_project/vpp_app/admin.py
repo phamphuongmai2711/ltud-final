a27af88a4a40601e123efc8d548647852a56d22c
spatziseoi <27a4012305@hvnh.edu.vn>
2026-05-17 22:12:23 +0700

    Update admin.py
    xóa code dòng thứ 3 đi

--- vpp_project/vpp_app/admin.py
+++ vpp_project/vpp_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Product, ContactLead
-
+from . import model
 # Đăng ký model để hiển thị trên trang admin
 admin.site.register(Product)
 admin.site.register(ContactLead)
