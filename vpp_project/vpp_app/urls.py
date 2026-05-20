21275c41fb6f52232ec606a77d6c8d807044cbb1
Phạm Phương Mai <phamphuongmai2711@gmail.com>
2026-05-20 15:21:43 +0700

    Update urls.py

--- vpp_project/vpp_app/urls.py
+++ vpp_project/vpp_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('san-pham/', views.product_list, name='product_list'), 
     path('tin-tuc/', views.news, name='news'),
     path('lien-he/', views.contact, name='contact'), 
+    path('dashboard/', views.admin_dashboard, name='admin_dashboard'),
 ]
