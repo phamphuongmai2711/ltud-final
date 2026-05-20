568deae9c666567ccbd8a3b9b8c3227e9fea1060
Phạm Phương Mai <phamphuongmai2711@gmail.com>
2026-05-20 11:02:06 +0700

    Update admin.py

--- vpp_project/products/admin.py
+++ vpp_project/products/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
-from .models import Product
-
+from .models import Product, ContactLead
+from . import models
+# Đăng ký model để hiển thị trên trang admin
 admin.site.register(Product)
+admin.site.register(ContactLead)
